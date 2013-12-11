bf5afe585304384e2b7e47b143fd4e914eb6bbf9
sbondada <sbondada@buffalo.edu>
2013-12-10 23:32:16 -0500

    added the code for performance metric and crossvalidation and rectified slight errors

--- Nearest_Neighbour.py
+++ Nearest_Neighbour.py
@@ -59,24 +59,87 @@ def nearest_neighbour(temptrainpoint_list,testpoint,k):
 
 def findclass(neighbourlist,testpoint):
         majoritylist=dict()
+        print "neighboring list "+str(len(neighbourlist))
         for i in range(len(neighbourlist)):
             if majoritylist.has_key(neighbourlist[i][-1]):
                 majoritylist[neighbourlist[i][-1]]=majoritylist[neighbourlist[i][-1]]+1
             else:
                 majoritylist[neighbourlist[i][-1]]=1
         sorted_majoritylist=sorted(majoritylist.items(),key=lambda x: x[1],reverse=True)
-        print sorted_majoritylist
+        print "majority list "+str(sorted_majoritylist)
         return sorted_majoritylist[0][0]
     
+def calculate_confusionlist(actual_labellist,predicted_labellist):
+    TP , FN , FP , TN = 0 , 0 , 0 , 0
+    for inc in range(len(predicted_labellist)):
+        if actual_labellist[inc]==1.0:
+            if predicted_labellist[inc]==1.0:
+               TP+=1
+            elif predicted_labellist[inc]==0.0:
+                FN+=1
+        elif actual_labellist[inc]==0.0:
+            if predicted_labellist[inc]==1.0:
+                FP+=1
+            if predicted_labellist[inc]==0.0:
+                TN+=1
+    return list((TP,FN,FP,TN)) 
+
+def calculateperformancemetric(confusion_list,metric):
+    a,b,c,d=(confusion_list)
+    if metric=="accuracy":
+        accuracy=float(a+d)/(a+b+c+d)
+        return accuracy
+    if metric=="precision":
+        try:
+            precision=float(a)/(a+c)
+        except:
+            precision=None
+        return precision
+    if metric=="recall":
+        try:
+            recall=float(a)/(a+b)
+        except:
+            recall=None
+        return recall
+    if metric=="fmeasure":
+        try:
+            p=float(a)/(a+c)
+            r=float(a)/(a+b)
+            fmeasure=float(2*r*p)/(r+p)
+        except:
+            fmeasure=None
+        return fmeasure
+
 if __name__ == '__main__':
     global point_list
     point_list=[]
-    loaddata('/home/kaushal/Ubuntu One/subjects/semester_3/DATA_MINING/project3/project3_dataset2.txt')
+    loaddata('/home/kaushal/Ubuntu One/subjects/semester_3/DATA_MINING/project3/project3_dataset1.txt')
     normalizedata()
-    print max(max(point_list,key=lambda x:max(x)))
-    trainpoint_list=copy.deepcopy(point_list[3:20])
-    testpoint_list=copy.deepcopy(point_list[20:40])
-    emp=nearest_neighbour(trainpoint_list,testpoint_list[2],4)
-    print emp
-    clas=findclass(emp,testpoint_list[2])
-    print clas
+    #print max(max(point_list,key=lambda x:max(x)))
+    looplist=range(0,len(point_list),len(point_list)/10)
+    looplist.pop(-1)
+    looplist.append(len(point_list))
+    accuracylist=[]
+    precisionlist=[]
+    recalllist=[]
+    fmeasurelist=[]
+    for inc in range(len(looplist)-1):
+        trainpoint_list_first=point_list[:looplist[inc]]
+        testpoint_list=point_list[looplist[inc]:looplist[inc+1]]
+        trainpoint_list_second=point_list[looplist[inc+1]:]
+        trainpoint_list=trainpoint_list_first+trainpoint_list_second
+        predictedlabellist=[]
+        for testitem in testpoint_list:
+            nnlist=nearest_neighbour(copy.deepcopy(trainpoint_list),testitem,5)
+            predictedlabel=findclass(nnlist,testpoint_list[2])
+            predictedlabellist.append(predictedlabel)
+        confusionlist=calculate_confusionlist(list(zip(*testpoint_list)[-1]),predictedlabellist)
+        accuracylist.append(calculateperformancemetric(confusionlist,"accuracy"))
+        precisionlist.append(calculateperformancemetric(confusionlist,"precision"))
+        recalllist.append(calculateperformancemetric(confusionlist,"recall"))
+        fmeasurelist.append(calculateperformancemetric(confusionlist,"fmeasure"))
+    print "accuracy for each fold "+str(accuracylist)
+    print "average accuracy"+str(float(sum(accuracylist))/len(accuracylist))
+    print "precision for each fold " + str(precisionlist)
+    print "recall for each fold " + str(recalllist)
+    print "fmeasure for each fold "+ str(fmeasurelist)
